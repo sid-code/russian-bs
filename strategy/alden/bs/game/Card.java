3a65310227fd6a23848662b1bcd9b6dd3fd6db7b
Alden Rogers <aldenrogers0@gmail.com>
2015-08-03 15:56:53 -0700

    Add Rank and Suit getters to Card class

--- strategy/alden/bs/game/Card.java
+++ strategy/alden/bs/game/Card.java
@@ -96,6 +96,14 @@ public class Card {
         this(rankFromString(s), suitFromString(s));
     }
 
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
     @Override
     public String toString() {
         return rank.toString() + suit.toString();
