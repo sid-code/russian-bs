473a37d5b638042d31d3d5c27293a594b1c6f4fc
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2015-08-05 23:01:47 -0700

    Check the current run for before playing lies

--- strategy/sid/lie_sometimes.nim
+++ strategy/sid/lie_sometimes.nim
@@ -37,6 +37,17 @@ proc checkLastPlay(game: RBSGame, cardsInHand: seq[Card]): bool =
 
     return knownNumber + cnum <= 4
 
+# See how many cards of a certain rank have been just put down
+proc findCurrentRun(game: RBSGame, rank: Rank): int =
+  result = 0
+  var index = 1
+  while true:
+    let (who, action) = game.history[^index]
+    if action.kind != aPlay or action.play.rank != rank: break
+
+    result += action.play.number
+    index += 1
+    if index >= game.history.len: break
 
 proc calculateMove(game: RBSGame, hand: seq[Card]): string =
   if hand.len == 0:
@@ -67,9 +78,22 @@ proc calculateMove(game: RBSGame, hand: seq[Card]): string =
       # The actual card that we'll place down is going to be a card
       # that we have another of. If there's only one 3 in the hand, don't
       # place it because that might get us in this situation again.
+      #
+      # There will also be a probability of just making a call. However,
+      # the best call to make in this case is BELIEVE because it was
+      # already determined that BS isn't a great call.
+      #
+      # One more thing to consider is that lying about a card when people
+      # have already claimed to place 4 down is a bad idea because the
+      # person next may not have the card and be forced to call BS. Even
+      # worse, they have the card and call BS and get to play it as well.
+
 
       let numberInDiscardPile = game.discardPile.findRank(rank).len
-      if numberInDiscardPile < 3:
+      let numberAlreadyClaimed = findCurrentRun(game, rank)
+      let randomFactor = random(1.0)
+
+      if numberInDiscardPile < 3 and numberAlreadyClaimed < 4 and randomFactor < chanceToNotLie:
         let cardToPlace = findAppropriateCard(hand)
         return makePlay(@[cardToPlace], rank)
       else:
